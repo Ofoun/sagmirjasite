f6f0674af7961c22412ada7caac7e7d286af9c12
Jean-Marie TOWANOU <ofoun@yahoo.fr>
2023-11-06 20:21:18 +0100

    Add existing project files to Git

---
+++ src/main/java/com/ofoun/sagmirja/SagmirjawebApplication.java
@@ -0,0 +1,13 @@
+package com.ofoun.sagmirja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagmirjawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagmirjawebApplication.class, args);
+	}
+
+}
