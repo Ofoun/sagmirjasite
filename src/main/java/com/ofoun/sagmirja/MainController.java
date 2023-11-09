5cddefd1367ad1a74ff28561277ef1014aae8167
Jean-Marie TOWANOU <ofoun@yahoo.fr>
2023-11-09 09:45:47 +0100

    Add existing project files to Git

--- src/main/java/com/ofoun/sagmirja/MainController.java
+++ src/main/java/com/ofoun/sagmirja/MainController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-	@GetMapping("/*")
+	@GetMapping("/")
 	public String viewHomePage() {
 		
 		
-		return "/index.html";
+		return "/index";
 		
 	}
 
